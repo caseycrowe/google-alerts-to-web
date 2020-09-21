678b6ea520a7d6db4d1fc80f12ed5a08971d2d39
caseycrowe <casey.crowe@gmail.com>
2020-09-20 23:03:56 -0400

    Update filter-feeds.py
    Modified to download the newest copy of a blacklist hosted on the website. It can be commented out to simply open and use a local list of blacklisted domains. 
    
    news-admin.php appends selected domains to the blacklist in the folder it resides in. This script downloads that blacklist, allowing for an easier method than hand entering domains.
    
    Retrieving the blacklist also logs the action to feedlog.txt

--- filter-feeds.py
+++ filter-feeds.py
@@ -1,5 +1,6 @@
-# Remove articles that come from useless aggregator websites or other irrelevant sites that show up in the feed
+# Remove articles that come from useless aggregator websites
 
+import requests
 import pandas as pd
 import datetime
 
@@ -7,13 +8,19 @@ import datetime
 currentDT = datetime.datetime.now()
 cleanDate = currentDT.strftime("%Y-%m-%d %H:%M:%S")
 
+# Download the newest blacklist from the website
 
-#Create a Python list of the blacklisted websites to filter out:
+url = "https://somedomain.com/blacklist"
+r = requests.get(url, allow_redirects=False)
+open('blacklist.txt', 'wb').write(r.content)
 
-blacklist = [
-	'somewebsite.com',
-	'someotherwebsite.com'
-	]
+# Log blacklist download/update
+log = open('feedlog.txt', 'a')
+log.write(cleanDate + " The blacklist has been updated.\n")
+
+# Import blacklist.txt (a simpe text file, one domain entry per line)
+with open("blacklist.txt") as f:
+    blacklist = f.read().splitlines()
 
 # Open the .csv
 # Data Frame with *all* articles/links:
@@ -26,6 +33,5 @@ fdf = df[~df['post.link'].str.contains('|'.join(blacklist))]
 fdf.to_csv('feed-articles.csv', index=False)
 
 # Log the script running
-log = open('feedlog.txt', 'a')
 log.write(cleanDate + " The feed-articles.csv file has been filtered.\n")
 log.close()
